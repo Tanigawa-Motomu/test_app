9989bc3f4beaa000b13656c4fbfbfb92a59e9369
Tanigawa-Motomu <m_tanigawa@libertyfish.co.jp>
2025-03-11 16:32:58 +0900

    test7

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_072156) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
