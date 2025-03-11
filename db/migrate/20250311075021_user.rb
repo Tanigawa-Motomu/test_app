3b93ae6e8a0c861c5ac88ed4359f426f39d9b3d3
Tanigawa-Motomu <m_tanigawa@libertyfish.co.jp>
2025-03-11 17:05:42 +0900

    test

--- db/migrate/20250311075021_user.rb
+++ db/migrate/20250311075021_user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
+    create_table :users, if_not_exists: true do |t|
       t.string :name
       t.date :birth_day
       t.integer :gender
