a53db75d5e37d4c15efc8f20de56e5bc7f2b97ef
Tanigawa-Motomu <m_tanigawa@libertyfish.co.jp>
2025-03-11 16:59:20 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
     has_many :post_content, dependent: :destroy
 
     validates :name, presence: true
